a6050f43d540e2f3536223d66f829505117c312b
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-05-01 19:04:34 +0400

    test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-        <Route path="/" element={<Navigate to="/cart" />} />
+        <Route path="/" element={<Navigate to="/React-Ecommerce-Website" />} />
           <Route path="/React-Ecommerce-Website"  element={<Shop />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
