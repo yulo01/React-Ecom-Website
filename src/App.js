372102bf7534108bd02d4c1712b4ba84092a2127
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-05-01 15:02:45 +0400

    change path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Shop />} />
+          <Route path="/React-Ecommerce-Website" element={<Shop />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
